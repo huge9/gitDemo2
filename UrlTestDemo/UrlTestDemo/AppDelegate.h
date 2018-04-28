a7d1da795cea0862a536f762468269c098e37b3f
北京－王秋丽 <wangqiuli@jd.com>
2018-04-28 18:52:35 +0800

    inti gitDemo Project

---
+++ UrlTestDemo/UrlTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UrlTestDemo
+//
+//  Created by wql on 2018/4/26.
+//  Copyright © 2018年 wql. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
